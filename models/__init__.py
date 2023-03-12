7cbe4f56a48a67fa6545b2ab37bf1c66eb29625e
Danladi Etu <danladietugithub@gmail.com>
2023-03-12 14:09:54 -0700

    code_fix

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,6 @@
 #from models.base_model import BaseModel
 from models.engine import file_storage
 
+__all__ = ["city.py", "place.py", "amenity.py", "review.py", "state.py", "user.py", "base_model.py"]
 storage = file_storage.FileStorage()
 storage.reload()
